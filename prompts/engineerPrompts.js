bcfe2dcb117564d71f92ddb393f5f49a2754bff0
Philip Burrows <48831325+philManWithBeard@users.noreply.github.com>
2024-01-26 22:30:07 +0000

    import prompts for different roles

---
+++ prompts/engineerPrompts.js
@@ -0,0 +1,68 @@
+import inquirer from "inquirer";
+
+const engineerPrompts = () => {
+  return inquirer.prompt([
+    {
+      type: "input",
+      name: "name",
+      message: "What's the team managers name?",
+      // was there a response, if not then return false
+      validate: (response) => {
+        if (response) {
+          return true;
+        } else {
+          console.log("That didn't work. Re-enter your managers name.");
+          return false;
+        }
+      },
+    },
+    {
+      type: "input",
+      name: "employeeId",
+      message: "What's the managers Employee ID?",
+      // was there a response, if not then return false
+      validate: (response) => {
+        if (response) {
+          return true;
+        } else {
+          console.log("That didn't work. Re-enter your managers Employee ID.");
+          return false;
+        }
+      },
+    },
+    {
+      type: "input",
+      name: "email",
+      message: "What's the team managers email address?",
+      // was there a response, if not then return false
+      validate: (response) => {
+        if (response) {
+          return true;
+        } else {
+          console.log(
+            "That didn't work. Re-enter your managers email address."
+          );
+          return false;
+        }
+      },
+    },
+    {
+      type: "input",
+      name: "officeNumber",
+      message: "What's the team managers office number?",
+      // was there a response, if not then return false
+      validate: (response) => {
+        if (response) {
+          return true;
+        } else {
+          console.log(
+            "That didn't work. Re-enter your managers office number."
+          );
+          return false;
+        }
+      },
+    },
+  ]);
+};
+
+export default engineerPrompts;
