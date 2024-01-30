9a397cc996f9f042972ec5e2ceca9e90a64930f9
Philip Burrows <48831325+philManWithBeard@users.noreply.github.com>
2024-01-30 18:18:48 +0000

    manager prompts

--- index.js
+++ index.js
@@ -2,34 +2,17 @@ import Manager from "./lib/Manager.js";
 import Engineer from "./lib/Engineer.js";
 import Intern from "./lib/Intern.js";
 import inquirer from "inquirer";
+import figlet from "figlet";
 // const inquirer = require("inquirer");
 import path from "path";
 import fs from "fs";
+import managerPrompts from "./prompts/managerPrompts.js";
 
 // const OUTPUT_DIR = path.resolve(__dirname, "output");
 // const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 import render from "./src/page-template.js";
 // const render = require("./src/page-template.js");
-
-const managerPrompts = () => {
-  return inquirer.prompt([
-    {
-      type: "input",
-      name: "title",
-      message: "What's the name of your project?",
-      // was there a response, if not then return false
-      validate: (response) => {
-        if (response) {
-          return true;
-        } else {
-          console.log("That didn't work. Re-enter your project title.");
-          return false;
-        }
-      },
-    },
-  ]);
-};
 
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
 
