46e9b90836225e1f269b78cbfc9af6ad0fad7b0d
Philip Burrows <48831325+philManWithBeard@users.noreply.github.com>
2024-01-30 18:26:42 +0000

    intern questions

--- prompts/internPrompts.js
+++ prompts/internPrompts.js
@@ -6,13 +6,19 @@ const internPrompts = () => {
       type: "input",
       name: "intern_name",
       message: "What is your intern's name?",
+      validate: function (value) {
+        if (isNaN(value))
+          return "That didn't work. Re-enter your intern's name";
+        else return true;
+      },
     },
     {
       type: "input",
       name: "intern_id",
       message: "What is your intern's id?",
       validate: function (value) {
-        if (isNaN(value)) return " Please enter valid ID";
+        if (isNaN(value))
+          return "That didn't work. Please enter your intern's ID";
         else return true;
       },
     },
@@ -21,16 +27,20 @@ const internPrompts = () => {
       name: "intern_email",
       message: "What is your intern's email?",
       validate: function (value) {
-        valid = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(value);
-
-        if (valid) return true;
-        else return "  Please enter valid email ID ";
+        if (isNaN(value))
+          return "That didn't work. Please enter your intern's email";
+        else return true;
       },
     },
     {
       type: "input",
       name: "intern_school",
       message: "What is your intern's school?",
+      validate: function (value) {
+        if (isNaN(value))
+          return "That didn't work. Please enter your intern's school";
+        else return true;
+      },
     },
   ]);
 };
